8eac048124e11bc3d80ab0bd31b5e40d82665b22
hamza diniawi <60050013+hamzadiniawi@users.noreply.github.com>
2025-02-22 16:49:39 +0100

    Commit -v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import CarouselRatio from './my-components/carousel.js'
+import AppBar from './my-components/app-bar.js'
+import Card from './my-components/card.js'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+      <AppBar />
       </header>
-    </div>
+      <div className="App">
+        <CarouselRatio />
+      </div>
+      <br/>
+      <div>
+        <Card/>
+      </div>
+    </>
   );
 }
-
-export default App;
